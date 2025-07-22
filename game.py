2990ea858661ac619eb78655d769e47ba8415857
Nurefşan Akerik <153921109+nununa24@users.noreply.github.com>
2025-07-22 18:16:21 +0300

    Update game.py

--- game.py
+++ game.py
@@ -5,13 +5,15 @@ import random
 print(art.logo)
 A = random.choice(game_data.data)
 control = True
+score = 0
 while control:
     print('Compare A:',A['name'],',',A['description'],',',A['country'])
     print(art.vs)
     B = random.choice(game_data.data)
+    if A == B:
+        B = random.choice(game_data.data)
     print('Against B:',B['name'],',',B['description'],',',B['country'])
     choice = input("Who has more followers? Type 'A' or 'B': ").upper()
-    score = 0
     if (A['follower_count'] > B['follower_count'] and choice == 'A') or (A['follower_count'] < B['follower_count'] and choice == 'B'):
         score += 1
         print(art.logo)
@@ -20,4 +22,3 @@ while control:
     else:
         print(f"Sorry, that's wrong. Final score:{score}")
         control = False
-
